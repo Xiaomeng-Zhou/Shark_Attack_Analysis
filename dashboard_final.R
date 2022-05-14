4ff5c6323460cdd311cc16b3906f528fb5741a39
Xiaomeng Zhou <xzhou9@uncc.edu>
2022-05-14 12:03:11 -0400

    change pickerInput()

--- dashboard_final.R
+++ dashboard_final.R
@@ -109,13 +109,16 @@ ui <- dashboardPage(
                   # select country/place
                   pickerInput(inputId = "country", label = h4(strong("Country/Place")),
                               choices = countries,selected = "All place",
-                              options = list(`live-search`=TRUE)),
+                              options = pickerOptions(liveSearch = TRUE
+                                            )),
+                  
                   hr(),
                   
                   # select shark species 
                   pickerInput(inputId = "species", label = h4(strong("Select shark species")),
                               choices =  shark_species, selected = "All species",
-                              options = list(`live-search`=TRUE)),
+                              options = pickerOptions(liveSearch = TRUE
+                                         )),
                    hr(),
                   
                   ## select activity
